f6045eca07de8e994d38b7c78f833cd292c8aee9
animation <763678968@qq.com>
2019-12-13 22:31:27 +0800

    为代码采集模块增加断路器

--- index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
+++ index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
@@ -1,17 +1,16 @@
 package cn.how2j.trend.web;
 
-import cn.how2j.trend.pojo.Index;
-import cn.how2j.trend.service.IndexService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import cn.how2j.trend.pojo.Index;
+import cn.how2j.trend.service.IndexService;
 
 @RestController
 public class IndexController {
-
     @Autowired
     IndexService indexService;
 
